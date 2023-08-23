6178c0e1d1c9d5c7a758116b624de64f88d7128d
Sawaraa <daniellakotlar@gmail.com>
2023-08-23 19:32:48 +0300

    final.03 commit

--- src/com/lgs/lab/interface2/MyCalculator.java
+++ src/com/lgs/lab/interface2/MyCalculator.java
@@ -7,9 +7,7 @@ public class MyCalculator extends Menu implements Numerable{
 
   @Override
   public void devide() {
-    if( ( firstNumber == 0 && secondNumber == 0)
-        || (firstNumber == 0 && secondNumber !=0)
-        || (firstNumber != 0 && secondNumber == 0)){
+    if(secondNumber == 0){
       System.out.println("На нуль ділити не можна");
     }else {
       int devide = firstNumber / secondNumber;
